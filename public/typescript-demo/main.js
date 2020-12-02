57b3f41e6267fde73f9ed053ea29b501e5331045
djoshuac <c.joshua.dawson@gmail.com>
2020-12-02 04:07:29 -0700

    remove demo

--- public/typescript-demo/main.js
+++
@@ -1,8 +0,0 @@
-import { woopah } from "./fish";
-const fishy = {
-    hasScales: true,
-    size: 12,
-    name: "Harry",
-};
-console.log(fishy.name, "iis", fishy.size, "big");
-woopah(fishy.name);
